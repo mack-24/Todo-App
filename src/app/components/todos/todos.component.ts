ae9bd4afc279f8a03aea83aefca25a0bc9bdf955
Vinayak <navghanevinayak24@gmail.com>
2021-08-24 00:06:15 +0530

    First Commit

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from 'src/app/service/todo.service';
+
+import { Todo } from "./../../model/Todo";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+
+
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+ 
+  faTrashAlt=faTrashAlt;
+  todos: Todo[] | undefined ;
+
+  constructor(private todoService : TodoService) { }
+
+  ngOnInit(): void {
+    this.todoService.getTodos().subscribe(todos => {
+        this.todos=todos;
+    });
+  }
+
+changeTodoStatus(todo: Todo){
+  this.todoService.chnageStatus(todo);
+}
+
+deleteTodo(todo: Todo){
+  this.todoService.deleteTodo(todo);
+}
+
+}
